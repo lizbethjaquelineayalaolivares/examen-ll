e0e3b5f4877727074f6ea1602c5f33eda1454e42
lizbethjaquelineayalaolivares <20203tn007@utez.edu.mx>
2021-06-25 17:15:38 -0500

    first commit

---
+++ js/examen.js
@@ -0,0 +1,4 @@
+const formBuscar = () => {
+    let search = document.getElementById("search").value;
+    alert(`la busquera con nombre ${search} no se encuentra, por lo cual intente mas tarde`);
+}
